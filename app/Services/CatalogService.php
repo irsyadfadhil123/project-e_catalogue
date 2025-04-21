ed262302a9b467a7e307c76d0584b306f3e39972
Irsyad Fadhil <irsyadfadhil123@gmail.com>
2025-04-21 11:23:38 +0700

    update api endpoint

--- app/Services/CatalogService.php
+++ app/Services/CatalogService.php
@@ -8,30 +8,30 @@ class CatalogService
 {
     public function getProducts()
     {
-        $response = Http::withToken(env('CATALOG_API_KEY'))->get('http://127.0.0.1:8000/api/products');
+        $response = Http::withToken(env('CATALOG_API_KEY'))->get(env('CATALOG_API_ENDPOINT') . '/api/products');
 
         return $response->successful() ? $response->json() : [];
     }
 
     public function getProductById($id)
     {
-        $response = Http::withToken(env('CATALOG_API_KEY'))->get("http://127.0.0.1:8000/api/products/{$id}");
+        $response = Http::withToken(env('CATALOG_API_KEY'))->get(env('CATALOG_API_ENDPOINT') . "/api/products/{$id}");
 
         return $response->successful() ? $response->json() : [];
     }
 
     public function postProduct(array $data)
     {
-        return Http::withToken(env('CATALOG_API_KEY'))->post("http://127.0.0.1:8000/api/products", $data);
+        return Http::withToken(env('CATALOG_API_KEY'))->post(env('CATALOG_API_ENDPOINT') . "/api/products", $data);
     }
 
     public function updateProduct($id, array $data)
     {
-        return Http::withToken(env('CATALOG_API_KEY'))->put("http://127.0.0.1:8000/api/products/{$id}", $data);
+        return Http::withToken(env('CATALOG_API_KEY'))->put(env('CATALOG_API_ENDPOINT') . "/api/products/{$id}", $data);
     }
 
     public function deleteProduct($id)
     {
-        return Http::withToken(env('CATALOG_API_KEY'))->delete("http://127.0.0.1:8000/api/products/{$id}");
+        return Http::withToken(env('CATALOG_API_KEY'))->delete(env('CATALOG_API_ENDPOINT') . "/api/products/{$id}");
     }
 }
